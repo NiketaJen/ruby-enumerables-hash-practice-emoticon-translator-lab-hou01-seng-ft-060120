514591e0fca604642769222cf67f06a6ebf04d11
Niketa Jenerette <niketa.jenerette@gmail.com>
2020-05-25 04:54:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,6 +21,7 @@ def get_japanese_emoticon(file_path, emoticon)
   
   if emoticon == symbols[:english]
    return symbols[:japanese] 
+   binding.pry
  elsif
  emoticon == nil
    return message = "Sorry, that emoticon was not found"
